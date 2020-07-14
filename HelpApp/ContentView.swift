22f29ed4bb8d0648814083dae0d4a75edfb95377
abhigya wangoo <dnathunder@gmail.com>
2020-07-14 16:01:38 -0700

    Initial Commit

---
+++ HelpApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelpApp
+//
+//  Created by Abhigya Wangoo on 7/14/20.
+//  Copyright © 2020 Abhigya Wangoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
