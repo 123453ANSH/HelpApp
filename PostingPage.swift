20678efe54245be67b5505067baafb52adca0ffd
Ansh Verma <52272280+123453ANSH@users.noreply.github.com>
2020-07-29 21:25:37 -0700

    Update PostingPage.swift
    getting rid of unnecessary comments/variables

--- PostingPage.swift
+++ PostingPage.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2020 Ansh Verma. All rights reserved.
 //
 
+
 import SwiftUI
-//import FirebaseStorage
+import FirebaseFirestore
 
 
 struct Message {
@@ -30,24 +31,20 @@ extension View {
     }
 }
 
-struct PostingPage: View {
+struct SignUpPage: View {
     @State var msgContent = ""
     var user = ""
     var avatar = ""
-    @State private var isImportant = true;
-    @State private var data = ""
-    @State private var showImagePicker: Bool = false
+    @State private var isImportant = true
     @State private var image:Image? = nil
-    let messageData = ["Text", "Image", "Video"]
-    @State private var page = 0
-    // below is suppose to go after VStack
-    //insert the list of messages here
     var body: some View {
         NavigationView {
             VStack {
             Text("Please report your emergency here! You can write an explanation, provide pictures, and provide videos to detail the nature of your emergency. ")
+                
             TextPage()
                 .frame(width: 300, height: 300, alignment: .topLeading)
+                
             Spacer()
             HStack(spacing: 25) {
             NavigationLink(destination: ImagePage()) {
@@ -67,7 +64,7 @@ struct PostingPage: View {
                             .ButtonRender()
             
             Button(action: {
-                                          //  self.data = self.getDocument(self.addMessage(Message(content: self.msgContent, user: self.user, avatar: self.avatar), self.isImportant)) //adding the message on the buttonclick, as well as assigning the returnvalue to var data
+            //    self.addPosting(self.msgContent) // not uploading message - look into this and watch youtube video to fix
             }){
             Image(systemName: "paperplane")
                 .frame(width: 32.0, height: 32.0 )
@@ -76,50 +73,22 @@ struct PostingPage: View {
             }.padding(.bottom, 20)
             }
         .navigationBarTitle("Help App")
-        /*
-            VStack {
-                Text("hi")
-                //TextPage() // textpage on top, buttons for image and video below
-            HStack {
-                NavigationButton(destination:  TextPage()) {
-                    // not sure why NavigationButton is not being recognized
-                Text("Enter Image")
-                    .color(.white)
-                    .padding(12)
-            }
-                .ButtonRender()
-            .navigationBarTitle(Text("Image"))
-                
-            NavigationLink (destination: Text("Hey there")) {
-                Text("Enter Image")
-                    .color(.white)
-                    .padding(12)
-            }
-                .background(Color.red)
-                .cornerRadius(12)
-            .navigationBarTitle(Text("Image"))
-                
-            NavigationButton(destination: Text("Hey there")) {
-                Text("Enter Image")
-                    .color(.white)
-                    .padding(12)
-            }
-                .background(Color.red)
-                .cornerRadius(12)
-            .navigationBarTitle(Text("Image"))
-             }
-            }
-            */
-        /*
-            Picker("Choose what you would like to send in your help request", selection: $page) {
-                ForEach(0 ..< messageData.count) {
-                    Text("\(self.messageData[$0])%")
-                }
-            }.pickerStyle(SegmentedPickerStyle())
- */
-        }
- 
     }
+        func addPosting(_ msgContent: String){
+                let db = Firestore.firestore()
+        let user = db.collection("Emergency Messages").document("Ansh")
+                          user.setData(["Emergency message": "\(msgContent)"]) {
+                              (err) in
+                              
+                              if err != nil {
+                                  print((err?.localizedDescription)!)
+                                  return
+                              }
+                              print("success")
+                          }
+                  }
+        }
+
 
 struct TextView: UIViewRepresentable {
     // multiline comment/posting
@@ -132,7 +101,7 @@ struct TextView: UIViewRepresentable {
     func makeUIView(context: UIViewRepresentableContext<TextView>) -> UITextView {
         let textView = UITextView()
         
-      //  textView.textContainer.lineFragmentPadding = 0
+      
         textView.textContainerInset = .zero
         textView.font = UIFont.systemFont(ofSize: 17)
         
@@ -147,7 +116,7 @@ struct TextView: UIViewRepresentable {
         
         
         
-        //uiView.text = text // dont need
+    
         
         uiView.delegate = context.coordinator
     }
@@ -166,11 +135,7 @@ struct TextView: UIViewRepresentable {
         init(_ parent: TextView) {
             self.parent = parent
         }
-        /*
-        func textViewDidChange(_ textView: UITextView) {
-            parent.text = textView.text
-        } // dont need to override this
- */
+       
         func textViewDidBeginEditing(_ textView: UITextView) {
             if textView.textColor == .placeholderText {
                 textView.text = ""
@@ -232,12 +197,6 @@ struct ImagePage: View {
 
 struct TextPage: View {
     @State var msgContent = ""
-    var user = ""
-    var avatar = ""
-    @State private var isImportant = true;
-    @State private var data = ""
-    @State private var showImagePicker: Bool = false
-    @State private var image:Image? = nil
     var body: some View {
         NavigationView {
             ZStack {
@@ -249,17 +208,9 @@ struct TextPage: View {
 }
 
 
-/*struct MsgPage_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        MsgPage(contentMessage: "This is just the sample view ")
-    }
-}
-*/
-
-struct PostingPage_Previews: PreviewProvider {
-    static var previews: some View {
-        PostingPage()
-        //SignUpPage()
+        SignUpPage()
     }
 }
 
